7254c5d372bd266193073986330dd97eb959597a
esancak <73714315+esancak@users.noreply.github.com>
2021-02-17 22:44:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,12 @@ app.post('/users', (req, res) => {
     res.send(newUser);
 });
 
+app.put('/users/:id', (req, res) => {
+    let users = JSON.parse(fs.readFileSync('users.json'));
+
+    const user = users.find(u => parseInt(u.id) === parseInt(req.params.id));
+    if (!user) res.status(404).send("ID of user is not found");
+
 //missions - Beyza
 //rooms - Achelia
 //results - Achelia
